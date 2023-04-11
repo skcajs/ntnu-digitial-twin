d8d331d6066ac2d02b976356bdd25194286f9069
skcajs <sjackson864@outlook.com>
2023-04-11 15:17:58 +0200

    added boat model

--- ocean-scene/src/components/Boat.js
+++ ocean-scene/src/components/Boat.js
@@ -1,15 +1,85 @@
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { useLoader } from '@react-three/fiber';
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function Boat() {
-    const gltf = useLoader(GLTFLoader, "./origami_ships.glb");
+// function Boat() {
+//     const gltf = useLoader(GLTFLoader, "./origami_boat.glb");
+//     return (
+//         <>
+//             <primitive position={[0, 0.01, 0]} object={gltf.scene} scale={10} />
+//         </>
+//     );
+// };
+
+// export default Boat;
+
+
+
+import React, { useRef, useEffect, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import axios from "axios";
+
+export default function Model(props) {
+    const [data, setData] = useState({});
+    const group = useRef();
+    const { nodes, materials } = useGLTF("/origami_boat.glb");
+
+    const myReallyBadHardCodedAPIPath = "http://localhost:4999";
+
+    useEffect(() => {
+        axios.get(`${myReallyBadHardCodedAPIPath}/sim`).then(
+            function (result) {
+                const data = result;
+                setData(data.data);
+            }
+        );
+    });
+
+    useFrame(({ clock }) => {
+        const a = clock.getElapsedTime();
+        const key = String(Math.round(a * 10) / 10);
+        if (Object.keys(data).includes(key)) {
+            group.current.position.z = data[key]['x'] * 100;
+            group.current.position.x = data[key]['y'] * 100;
+            group.current.rotation.y = data[key]['psi'];
+        }
+    });
+
+
     return (
-        <>
-            <primitive position={[0, 0.01, 0]} object={gltf.scene} scale={10} />
-        </>
+        <group ref={group} {...props} dispose={null} scale={0.4}>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Plane015_Ships_0.geometry}
+                material={materials.Ships}
+            />
+        </group>
     );
-};
+}
 
-export default Boat;
+useGLTF.preload("/origami_boat.glb");
 
 
+// import React, { useRef } from "react";
+// import { useGLTF } from "@react-three/drei";
+
+// export default function Model(props) {
+//     const { nodes, materials } = useGLTF("/origami_boat.glb");
+//     return (
+//         <group {...props} dispose={null}>
+//             <group rotation={[-Math.PI / 2, 0, 0]} scale={0}>
+//                 <group rotation={[Math.PI / 2, 0, 0]}>
+//                     <mesh
+//                         castShadow
+//                         receiveShadow
+//                         geometry={nodes.Plane015_Ships_0.geometry}
+//                         material={materials.Ships}
+//                     />
+//                 </group>
+//             </group>
+//         </group>
+//     );
+// }
+
+// useGLTF.preload("/origami_boat.glb");
--- turning_vessel_model/app.py
+++ turning_vessel_model/app.py
@@ -1,22 +1,25 @@
 from flask import Flask
+from flask_cors import CORS
 import numpy as np
 from FE import predict
 from vessel import Vessel
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/sim")
 def hello_world():
     xtab_FE, ttab_FE = predict(
-        t_tot = 20, 
-        t = 0, 
-        dt = 0.1, 
-        u = np.array([[1],[-0.01]], dtype=float), 
-        vs= Vessel())
-    
-    
+        t_tot=20,
+        t=0,
+        dt=0.1,
+        u=np.array([[1], [-0.01]], dtype=float),
+        vs=Vessel())
+
     new_dict = {}
-    for i in range(10):
+
+    for i in range(len(ttab_FE)):
         inner_dict = {}
         inner_dict['x'] = xtab_FE[i][0][0]
         inner_dict['y'] = xtab_FE[i][1][0]
@@ -24,6 +27,11 @@ def hello_world():
         inner_dict['u'] = xtab_FE[i][3][0]
         inner_dict['c'] = xtab_FE[i][4][0]
         inner_dict['r'] = xtab_FE[i][5][0]
-        new_dict[ttab_FE[i]] = inner_dict
+        inner_dict['t'] = round(ttab_FE[i], 1)
+        new_dict[round(ttab_FE[i], 1)] = inner_dict
 
-    return(new_dict)
+    return (new_dict)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4999, debug=True)
